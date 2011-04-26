16835c91b401253bca8b1544860363a302ffd34e
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2011-04-26 03:50:23 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@20 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/app_model.php
+++ app/app_model.php
@@ -31,52 +31,34 @@
  * @subpackage    cake.app
  */
 class AppModel extends Model {
-  public function afterFind($results, $primary = false) 
-  {
-    if (method_exists($this, 'doAfterFind')) 
-    {
-      if ($primary) 
-      {
-        foreach ($results as $key => $val) 
-        {
-          if (isset($val[$this->alias])) 
-          {
-            $results[$key][$this->alias] = $this->doAfterFind($results[$key][$this->alias]);
-          }
-        }
-      } 
-      else 
-      {
-        if (isset($results[$this->primaryKey])) 
-        {
-          $results = $this->doAfterFind($results);
-        } 
-        else 
-        {
-          foreach ($results as $key => $val) 
-          {
-            if (isset($val[$this->alias])) 
-            {
-              if (isset($val[$this->alias][$this->primaryKey]))
-              {
-                $results[$key][$this->alias] = $this->doAfterFind($results[$key][$this->alias]);
-              } 
-              else 
-              {
-                foreach ($results[$key][$this->alias] as $key2=> $val2) 
-                {
-                  $results[$key][$this->alias][$key2] = 
-                  $this->doAfterFind($results[$key][$this->alias][$key2]);
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    
-    return $results;
-  }
-  
-  
+	public function afterFind($results, $primary =false) {
+		if(method_exists($this, 'doAfterFind')) {
+			if($primary) {
+				foreach($results as $key => $val) {
+					if(isset($val[$this -> alias])) {
+						$results[$key][$this -> alias] = $this -> doAfterFind($results[$key][$this -> alias]);
+					}
+				}
+			} else {
+				if(isset($results[$this -> primaryKey])) {
+					$results = $this -> doAfterFind($results);
+				} else {
+					foreach($results as $key => $val) {
+						if(isset($val[$this -> alias])) {
+							if(isset($val[$this -> alias][$this -> primaryKey])) {
+								$results[$key][$this -> alias] = $this -> doAfterFind($results[$key][$this -> alias]);
+							} else {
+								foreach($results[$key][$this->alias] as $key2 => $val2) {
+									$results[$key][$this -> alias][$key2] = $this -> doAfterFind($results[$key][$this -> alias][$key2]);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return $results;
+	}
+
 }
