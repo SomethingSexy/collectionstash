c96b17856313d78c104a8263d93172470e818273
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2011-04-27 02:05:19 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@24 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+$config['Settings'] = Configure::read('Settings');
+
+$config['Settings'] = Set::merge(ife(empty($config['Settings']), array(), $config['Settings']), array(
+  'version' => '1.0.213',
+  'title' => 'My Application',
+  'registration' => true,
+  'Approval' => array (
+    'auto-approve' => 'false'
+  ),
+  'Stash' => array (
+  	'total-allowed' => '5'
+  )
+));
+
+?>
