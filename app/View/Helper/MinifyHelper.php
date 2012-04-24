c0f3ea2600903b797d3eb8e5b7cf804dac151e2a
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-04-24 00:13:37 +0000

    Fixing defects with last release.
    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@363 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/View/Helper/MinifyHelper.php
+++ app/View/Helper/MinifyHelper.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /***
  * Cakephp view helper to interface with http://code.google.com/p/minify/ project.
  * Minify: Combines, minifies, and caches JavaScript and CSS files on demand to speed up page loads.
@@ -6,36 +6,34 @@
  * Requirements: An entry in core.php - "MinifyAsset" - value of which is either set 'true' or 'false'. False would be usually set during development and/or debugging. True should be set in production mode.
  */
 
-class MinifyHelper extends AppHelper{
-        
-        var $helpers = array('Javascript','Html'); //used for seamless degradation when MinifyAsset is set to false;
-        
-        function js($assets){
-            if(Configure::read('MinifyAsset')){
-               echo(sprintf("<script type='text/javascript' src='%s'></script>",$this->_path($assets, 'js')));
-            }
-            else{
-                echo($this->Javascript->link($assets));
-            }
-        }
-        
-        
-        function css($assets){
-            if(Configure::read('MinifyAsset')){
-                echo(sprintf("<link type='text/css' rel='stylesheet' href='%s' />",$this->_path($assets, 'css')));
-            }
-            else{
-                echo($this->Html->css($assets));
-            }
-        }
-        
-        function _path($assets, $ext){
-            $path = $this->webroot . "min/?f=";
-            foreach($assets as $asset){
-                $path .= ($asset . ".$ext,");
-            }
-            return substr($path, 0, count($path)-2);
-        }
-    }
+class MinifyHelper extends AppHelper {
 
+	var $helpers = array('Js', 'Html');
+	//used for seamless degradation when MinifyAsset is set to false;
+
+	function js($assets) {
+		if (Configure::read('MinifyAsset')) {
+			echo(sprintf("<script type='text/javascript' src='%s'></script>", $this -> _path($assets, 'js')));
+		} else {
+			echo($this -> Javascript -> link($assets));
+		}
+	}
+
+	function css($assets) {
+		if (Configure::read('MinifyAsset')) {
+			echo(sprintf("<link type='text/css' rel='stylesheet' href='%s' />", $this -> _path($assets, 'css')));
+		} else {
+			echo($this -> Html -> css($assets));
+		}
+	}
+
+	function _path($assets, $ext) {
+		$path = $this -> webroot . "min/?f=";
+		foreach ($assets as $asset) {
+			$path .= ($asset . ".$ext,");
+		}
+		return substr($path, 0, count($path) - 2);
+	}
+
+}
 ?> 
