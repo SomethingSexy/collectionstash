f422bb5fd960a670622b6c601877a40619f42532
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-04-28 02:06:39 +0000

    Adding back models with updated name because of cakephp 2.1.1 upgrade.
    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@382 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

---
+++ app/Model/Collectibletype.php
@@ -0,0 +1,25 @@
+<?php
+class Collectibletype extends AppModel {
+	var $name = 'Collectibletype';
+	var $hasMany = array('Collectible' => array('className' => 'Collectible', 'foreignKey' => 'collectibletype_id'), 'CollectibletypesManufacture');
+	var $actsAs = array('Tree','Containable');
+
+    public $validate = array(
+    //name field
+    'name' => array('minLength' => array('rule' => 'notEmpty', 'message' => 'Name is required.'), 'maxLength' => array('rule' => array('maxLength', 200), 'message' => 'Invalid length.')));
+
+
+	public function getCollectibleTypeSearchData() {
+		$collectibleTypes = $this -> find("all", array('order' => array('Collectibletype.name' => 'ASC'), 'contain' => false));
+		debug($collectibleTypes);
+		return $collectibleTypes;
+	}
+	
+	public function getListTopLevelTypes(){
+		$collectibleTypes = $this -> find("list", array('conditions'=> array('Collectibletype.parent_id'=> null)));	
+		
+		return $collectibleTypes;
+	}
+
+}
+?>
