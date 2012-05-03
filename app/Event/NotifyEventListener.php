cb28b5fdea2984009ad1375720ae4e22c782c529
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-05-03 23:38:43 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@393 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Event/NotifyEventListener.php
+++ app/Event/NotifyEventListener.php
@@ -3,25 +3,35 @@ App::uses('CakeEventListener', 'Event');
 class NotifyEventListener implements CakeEventListener {
 
 	public function implementedEvents() {
-		return array('Event.Subscription.trigger' => 'notifyUser',
-		);
+		return array('Model.Subscription.notify' => 'notifyUser', );
 	}
 
 	/**
-	 * This executes anytime that subscription gets triggered and we need to 
+	 * This executes anytime that subscription gets triggered and we need to
 	 * notify the user that something happened'
-	 * 
+	 *
 	 * This will get passed the user id and the message that we are going to
 	 * notify
 	 */
 	public function notifyUser($event) {
-		// $event -> data['userId'];
-		// $event -> data['message'];
-		
+		$subscriptions = $event -> data['subscriptions'];
+		$message = $event -> data['message'];
+
 		// We will be loading the Notify model and then updating it
 		// I think the subject will be the controller/model whatever the subscription was
 		// $event -> subject ->
-		
+
+		$event -> subject -> loadModel('Message');
+
+		// This could kick off a shit ton of events, depending on how many people.
+		// We might want to do a bulk notify
+		$data = array();
+		foreach ($subscriptions as $key => $subscription) {
+			array_push($data, array('user_id'=> $subscription['Subscription']['user_id'], 'message'=> $message));
+		}
+
+		$event -> subject -> Message -> saveAll($data);
+
 		CakeLog::write('info', 'Notify Bitches');
 	}
 
--- app/Model/Subscription.php
+++ app/Model/Subscription.php
@@ -1,6 +1,15 @@
 <?php
 /**
- *
+ * This model tells us what entities a user is subscribed to
+ * 
+ * Should we also indicate here, what they are subscribed to? Comments, the entity itself
+ * 
+ * For example
+ * 	- If you are subscribed to the Stash, anytime something is added, you will be notified
+ * 	- If you are subscribed to a collectible, anytime something is edited with that collectible, you will be notified.
+ * 	- Should you be notified of different comments that are made as well? 
+ * 
+ * 	- I think if your settings say that you auto suscribe when posting comments, it would just be for the comments so you would suscribe to that entity, but say comments only
  *
  */
 class Subscription extends AppModel {
