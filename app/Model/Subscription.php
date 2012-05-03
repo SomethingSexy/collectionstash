e629088ad52d124bd98d932ff7bcc7f4dd8e28a8
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-05-03 03:13:22 +0000

    Finished the latest comment loading logic.  Started to work on the notification system.
    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@392 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Model/Subscription.php
+++ app/Model/Subscription.php
@@ -5,7 +5,7 @@
  */
 class Subscription extends AppModel {
 	public $name = 'Subscription';
-	public $belongsTo = array('EntityType', 'User' => array('fields' => array('id', 'username')));
+	public $belongsTo = array('EntityType', 'User' => array('fields' => array('id', 'username', 'email')));
 	public $actsAs = array('Containable');
 
 }
