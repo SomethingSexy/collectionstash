0d5266ce8432dceeb53934115f706ef4c5fd03bd
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-05-04 00:35:00 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@395 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Model/Subscription.php
+++ app/Model/Subscription.php
@@ -11,6 +11,15 @@
  * 
  * 	- I think if your settings say that you auto suscribe when posting comments, it would just be for the comments so you would suscribe to that entity, but say comments only
  *
+ * 
+ * A subscription will indicate what they are subscribing to, the whole entity, comments or both
+ * 
+ * Because comments don't have their own entity, if someone wants to subscribe to the comments, we will have to code
+ * it in a way that they are subscribed to the entity but for the comments only.
+ * 
+ * Then when a comment has been posted for an entity, we will kick off an event and it will check the subcriptions for
+ * that entity that are specific to comments or both
+ * 
  */
 class Subscription extends AppModel {
 	public $name = 'Subscription';
