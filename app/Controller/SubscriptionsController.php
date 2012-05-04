65a4e96e76acfa66b312dc19acfb4f17b10024e3
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-05-04 00:19:53 +0000

    Back to calling this a notification
    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@394 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Controller/SubscriptionsController.php
+++ app/Controller/SubscriptionsController.php
@@ -2,13 +2,17 @@
 class SubscriptionsController extends AppController {
 	public $helpers = array('Html', 'Minify', 'Js');
 
-	public function test() {
-
-		// $subscriptions = $this -> Subscription -> find("all", array('contain' => 'EntityType', 'joins' => array('table' => 'entity_types', 'alias' => 'EntityType', 'type' => 'inner', 'conditions' => array('Subscription.entity_type_id = EntityType.id'))));
-		$entityType = $this -> Subscription -> EntityType -> find("first", array('contain'=> false, 'conditions' => array('EntityType.type' => 'stash', 'EntityType.type_id' => 1)));
-		debug($entityType);
-		$subscriptions = $this -> Subscription -> find("all", array('contain' => array('User'), 'conditions' => array('Subscription.entity_type_id' => $entityType['EntityType']['id'])));
-		debug($subscriptions);
+	// public function test() {
+	//
+	// // $subscriptions = $this -> Subscription -> find("all", array('contain' => 'EntityType', 'joins' => array('table' => 'entity_types', 'alias' => 'EntityType', 'type' => 'inner', 'conditions' => array('Subscription.entity_type_id = EntityType.id'))));
+	// $entityType = $this -> Subscription -> EntityType -> find("first", array('contain'=> false, 'conditions' => array('EntityType.type' => 'stash', 'EntityType.type_id' => 1)));
+	// debug($entityType);
+	// $subscriptions = $this -> Subscription -> find("all", array('contain' => array('User'), 'conditions' => array('Subscription.entity_type_id' => $entityType['EntityType']['id'])));
+	// debug($subscriptions);
+	// }
+	
+	public function subscribe(){
+		
 	}
 
 }
--- app/Model/Notification.php
+++ app/Model/Notification.php
@@ -11,9 +11,11 @@
  * 		- message
  * 		- or is message always on and you can't turn that off? Then maybe this should be a message object and the notification object tells me how to notify the user
  * 
- *
+ *  I might go back to calling this notifications
+ * 		- Not sure I want to confuse this when I create a message center/conversation center
+ * 		- You would get notified of a new message in your message center or a new conversation
  */
-class Message extends AppModel {
+class Notification extends AppModel {
 	public $name = 'Message';
 	public $belongsTo = array('User' => array('fields' => array('id', 'username', 'email')));
 	public $actsAs = array('Containable');
