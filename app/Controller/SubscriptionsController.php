882e89f8ac7b6ff4fa3675e09269e6e55954e660
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-11-23 21:52:43 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@638 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Controller/SubscriptionsController.php
+++ app/Controller/SubscriptionsController.php
@@ -2,47 +2,6 @@
 class SubscriptionsController extends AppController {
 	public $helpers = array('Html', 'Minify', 'Js');
 
-	public function test() {
-
-		// $subscriptions = $this -> Subscription -> find("all", array('contain' => 'EntityType', 'joins' => array('table' => 'entity_types', 'alias' => 'EntityType', 'type' => 'inner', 'conditions' => array('Subscription.entity_type_id = EntityType.id'))));
-		// $entityType = $this -> Subscription -> EntityType -> find("first", array('contain' => false, 'conditions' => array('EntityType.type' => 'stash', 'EntityType.type_id' => 1)));
-		// debug($entityType);
-		// $subscriptions = $this -> Subscription -> find("all", array('contain' => array('User'), 'conditions' => array('Subscription.entity_type_id' => $entityType['EntityType']['id'])));
-		// debug($subscriptions);
-		// $subscriptions = $this -> Subscription -> find("all", array('contain' => array('User'), 'conditions' => array('Subscription.subscribed' => 1, 'Subscription.entity_type_id' => 712)));
-		// $entityType = $this -> Subscription -> EntityType -> getEntityCore(712);
-		// debug($subscriptions);
-		//
-		// $stash = $this -> Subscription -> EntityType -> Stash -> find("first", array('contain' => array('User'), 'conditions' => array('Stash.id' => 58)));
-		// debug($stash);
-		// //now grab all of the Subscriptions
-		// $subscriptions = $this -> Subscription -> find("all", array('contain' => array('User'), 'conditions' => array('Subscription.subscribed' => 1, 'Subscription.entity_type_id' => 712)));
-		//
-		// if (!empty($subscriptions)) {
-		// //Build the message
-		// $message = $stash['User']['username'];
-		// $message .= __(' has added a new collectible to their stash!');
-		//
-		// foreach ($subscriptions as $key => $subscription) {
-		// //If the subscription is the same as the owner of the stash, unset it
-		// CakeLog::write('info', $subscription['Subscription']['user_id']);
-		// CakeLog::write('info', $stash['Stash']['user_id']);
-		// if ($subscription['Subscription']['user_id'] === $stash['Stash']['user_id']) {
-		// unset($subscriptions[$key]);
-		// } else {
-		// $subscriptions[$key]['Subscription']['message'] = $message;
-		// }
-		//
-		// }
-		//
-		// debug($subscriptions);
-		//
-		// } else {
-		//
-		// }
-
-	}
-
 	public function subscribe() {
 		$data = array();
 		//must be logged in to post comment
