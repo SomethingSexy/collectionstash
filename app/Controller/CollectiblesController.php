625eb2907989753125c9eae1a6cd2851b16b530a
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-12-18 03:01:24 +0000

    Performance testing
    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@708 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Controller/CollectiblesController.php
+++ app/Controller/CollectiblesController.php
@@ -828,7 +828,7 @@ class CollectiblesController extends AppController {
 			$this -> redirect(array('action' => 'index'));
 		}
 		// TODO: We really need to start caching collectibles I think...we are fetching A LOT of data
-		$collectible = $this -> Collectible -> find('first', array('conditions' => array('Collectible.id' => $id), 'contain' => array('Currency', 'SpecializedType', 'Manufacture', 'User' => array('fields' => 'User.username'), 'Collectibletype', 'License', 'Series', 'Scale', 'Retailer', 'CollectiblesUpload' => array('Upload'), 'CollectiblesTag' => array('Tag'), 'AttributesCollectible' => array('Revision' => array('User'), 'Attribute' => array('AttributeCategory', 'Manufacture', 'Scale', 'AttributesCollectible' => array('Collectible' => array('fields' => array('id', 'name')))), 'conditions' => array('AttributesCollectible.active' => 1)))));
+		$collectible = $this -> Collectible -> find('first', array('conditions' => array('Collectible.id' => $id), 'contain' => array('Currency', 'SpecializedType', 'Manufacture', 'User' => array('fields' => 'User.username'), 'Collectibletype', 'License', 'Series', 'Scale', 'Retailer', 'CollectiblesUpload' => array('Upload'), 'CollectiblesTag' => array('Tag'), 'AttributesCollectible' => array('Revision' => array('User'), 'Attribute' => array('AttributeCategory', 'Manufacture', 'Scale'), 'conditions' => array('AttributesCollectible.active' => 1)))));
 		debug($collectible);
 		if (!empty($collectible) && $collectible['Collectible']['state'] === '0') {
 			$this -> set('collectible', $collectible);
