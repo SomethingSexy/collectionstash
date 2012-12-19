9fcf7cf5d90de5c22432d9142a42d3026b31c8b6
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2012-12-19 02:02:17 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@712 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Console/Command/ProcessActivitiesShell.php
+++ app/Console/Command/ProcessActivitiesShell.php
@@ -7,26 +7,130 @@
  * This will run once every hour.
  */
 class ProcessActivitiesShell extends AppShell {
-	public $uses = array('Activity', 'UserPointsFact', 'Point');
+	public $uses = array('Activity', 'UserPointFact', 'Point');
 
 	public function main() {
 		// This grabs yesterdayd date
 		//array('Post.read_count BETWEEN ? AND ?' => array(1,10))
 
+		// This will have the numeric representation of the month
+		$month = date("m");
+		$day = date("d") - 1;
+		// This will have the year string
+		$year = date("Y");
+
 		// Supposedly this is the slowest but I am not too worried about it for now
-		$start = date("Y-m-d H:i:s", mktime(0, 0, 0, date("m"), date("d") - 1, date("Y")));
-		$end = date("Y-m-d H:i:s", mktime(23, 59, 59, date("m"), date("d") - 1, date("Y")));
+		$start = date("Y-m-d H:i:s", mktime(0, 0, 0, $month, $day, $year));
+		$end = date("Y-m-d H:i:s", mktime(23, 59, 59, $month, $day, $year));
 
 		$activities = $this -> Activity -> find('all', array('conditions' => array('Activity.created BETWEEN ? AND ?' => array($start, $end))));
 
 		$points = $this -> Point -> find('list', array('fields' => array('Point.activity_type_id', 'Point.points')));
 
-		debug($points);
+		debug($month);
 
 		foreach ($activities as $key => $activity) {
-			$score = $points[$activity['ActivityType']['id']];	
-			if()
+			$score = $points[$activity['ActivityType']['id']];
+			if ($score !== '0') {
+				$data = $activity['Activity']['data'];
+
+				$saveData = array();
+				$saveData['UserPointFact'] = array();
+				$saveData['UserPointFact']['month'] = $month;
+				$saveData['UserPointFact']['year'] = $year;
+
+				// Add comment
+				if ($activity['ActivityType']['id'] === '1') {
+					// user id
+					$userId = $data -> actor -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				// Add to Stash
+				else if ($activity['ActivityType']['id'] === '2') {
+					// user id
+					$userId = $data -> actor -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				// Remove From Stash
+				else if ($activity['ActivityType']['id'] === '3') {
+					// user id
+					$userId = $data -> actor -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				//Edit User Collectible
+				else if ($activity['ActivityType']['id'] === '4') {
+					// user id
+					$userId = $data -> actor -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				// Add Photo
+				else if ($activity['ActivityType']['id'] === '5') {
+					// user id
+					$userId = $data -> actor -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				// Approve New
+				else if ($activity['ActivityType']['id'] === '8') {
+					// user id
+					$userId = $data -> target -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				// Approve Edit
+				else if ($activity['ActivityType']['id'] === '9') {
+					// user id
+					$userId = $data -> target -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+				// invite
+				else if ($activity['ActivityType']['id'] === '10') {
+					// user id
+					$userId = $data -> actor -> id;
+					$saveData['UserPointFact']['user_id'] = $userId;
+				}
+
+				// Now that we have a score and who to add it to, let's see if we have something
+				// added already
+
+				$userFact = $this -> getFact($saveData['UserPointFact']['user_id'], $month, $year);
+
+				debug($userFact);
+				
+				$this -> updateFact($userFact, $score);
+
+			}
 		}
+	}
+
+	private function getUser($id) {
+		return $this -> User -> find('first', array('contain' => false, 'conditions' => array('User.id' => $id)));
+	}
+
+	private function getFact($userId, $month, $year) {
+		$userFact = $this -> UserPointFact -> find('first', array('contain' => false, 'conditions' => array('UserPointFact.user_id' => $userId, 'UserPointFact.month' => $month, 'UserPointFact.year' => $year)));
+		debug($userFact);
+		if (!$userFact) {
+			$saveData = array();
+			$saveData['UserPointFact'] = array();
+			$saveData['UserPointFact']['month'] = $month;
+			$saveData['UserPointFact']['year'] = $year;
+			$saveData['UserPointFact']['user_id'] = $userId;
+			$saveData['UserPointFact']['points'] = 0;
+			if ($this -> UserPointFact -> save($saveData)) {
+				$id = $this -> UserPointFact -> id;
+				$userFact = $this -> UserPointFact -> find('first', array('conditions' => array('UserPointFact.id' => $id)));
+			}
+		}
+
+		return $userFact;
+	}
+
+	private function updateFact($userFact, $score) {
+		// Since we are updating everyone one at a time and not in batch, I should be able to take the current
+		// one passed in, add the score and then save...the next time through we will be retrieving the
+		// fact again so that we can update it
+		$userFact['UserPointFact']['points'] = $userFact['UserPointFact']['points']  + $score;
+		
+		$this -> UserPointFact -> save($userFact);
 	}
 
 }
--- app/Model/UserPointsFact.php
+++
@@ -1,6 +0,0 @@
-<?php
-class UserPointsFact extends AppModel {
-    var $name = 'UserPointsFact';
-    var $actsAs = array('Containable');
-}
-?>
