7846fc8c954836d2958b95153a6d7111126dd3de
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2013-02-10 20:37:46 +0000

    Fixes 2.1.1
    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@877 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Config/config.php
+++ app/Config/config.php
@@ -2,7 +2,7 @@
 $config['Settings'] = Configure::read('Settings');
 
 $config['Settings'] = Set::merge($config['Settings'], array(
-	'version' => '2.1',
+	'version' => '2.1.1',
   	'title' => 'My Application',
   	'registration' => array(
   		'open' => true,
