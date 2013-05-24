079d71c616393791790e82204a8db35562d95318
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2013-05-24 01:42:48 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@1172 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Model/Transaction.php
+++ app/Model/Transaction.php
@@ -6,5 +6,45 @@ class Transaction extends AppModel {
 
 	public $validate = array();
 
+	function afterFind($results, $primary = false) {
+		if ($results) {
+			// If it is primary handle all of these things
+			if ($primary) {
+				foreach ($results as $key => $val) {
+					if (isset($val['Transaction'])) {
+						if (isset($val['Transaction']['sale_date'])) {
+							$datetime = strtotime($val['Transaction']['sale_date']);
+							$datetime = date("m/d/y g:i A", $datetime);
+							$results[$key]['Transaction']['sale_date'] = $datetime;
+						}
+					}
+				}
+			} else {
+				if (isset($results[$this -> primaryKey])) {
+
+					if (isset($results['sale_date'])) {
+						$datetime = strtotime($results['sale_date']);
+						$datetime = date("m/d/y g:i A", $datetime);
+						$results['sale_date'] = $datetime;
+					}
+				} else {
+
+					foreach ($results as $key => $val) {
+
+						if (isset($val['Transaction'])) {
+							if (isset($val['Transaction']['sale_date'])) {
+								$datetime = strtotime($val['Transaction']['sale_date']);
+								$datetime = date("m/d/y g:i A", $datetime);
+								$results[$key]['Transaction']['sale_date'] = $datetime;
+							}
+						}
+					}
+				}
+			}
+
+		}
+		return $results;
+	}
+
 }
 ?>
