746cc9766030ecbc68f6b925d8208028e14bf737
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2013-06-01 04:26:01 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@1198 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/Config/config.php
+++ app/Config/config.php
@@ -2,7 +2,7 @@
 $config['Settings'] = Configure::read('Settings');
 
 $config['Settings'] = Set::merge($config['Settings'], array(
-	'version' => '2.5.0.1',
+	'version' => '2.5.1',
   	'title' => 'My Application',
   	'registration' => array(
   		'open' => true,
