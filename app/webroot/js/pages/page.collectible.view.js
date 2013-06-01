088ddcf532bda489521c479166f5623578d1aeb2
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2013-06-01 02:40:44 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@1195 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/webroot/js/pages/page.collectible.view.js
+++ app/webroot/js/pages/page.collectible.view.js
@@ -45,12 +45,52 @@ $(function() {
 		}).render().el);
 
 		var stashAddView = null;
+		var collectibleUser = null;
+		
+		$('#stash-add-dialog', 'body').on('hidden', function() {
+			stashAddView.remove();
+		});
 
-		// TODO: We need to do a call to see if the user
-		// akready owns this guy
+		$('.save', '#stash-add-dialog').click(function() {
+			collectibleUser.save({}, {
+				success : function(model, response, options) {
+					if (response.response.isSuccess) {
+						$('#stash-add-dialog').modal('hide');
+						var message = 'You have successfully added the collectible to your stash!';
+
+						$.blockUI({
+							message : '<button class="close" data-dismiss="alert" type="button">×</button>' + message,
+							showOverlay : false,
+							css : {
+								top : '100px',
+								'background-color' : '#DDFADE',
+								border : '1px solid #93C49F',
+								'box-shadow' : '3px 3px 5px rgba(0, 0, 0, 0.5)',
+								'border-radius' : '4px 4px 4px 4px',
+								color : '#333333',
+								'margin-bottom' : '20px',
+								padding : '8px 35px 8px 14px',
+								'text-shadow' : '0 1px 0 rgba(255, 255, 255, 0.5)',
+								'z-index' : 999999
+							},
+							timeout : 2000
+						});
+					} else {
+						if (response.response.errors) {
+							stashAddView.errors = response.response.errors;
+							stashAddView.render();
+						}
+					}
+				},
+				error : function(model, xhr, options) {
+
+				}
+			});
+
+		});
 
 		$('.add-stash-link').click(function(event) {
-			var collectibleUser = new CollectibleUserModel({
+			collectibleUser = new CollectibleUserModel({
 				'collectible_id' : collectibleModel.get('id')
 			});
 
@@ -67,47 +107,6 @@ $(function() {
 			});
 
 			$('.modal-body', '#stash-add-dialog').html(stashAddView.render().el);
-
-			$('#stash-add-dialog', 'body').on('hidden', function() {
-				stashAddView.remove();
-			});
-			$('.save', '#stash-add-dialog').click(function() {
-				collectibleUser.save({}, {
-					success : function(model, response, options) {
-						if (response.response.isSuccess) {
-							$('#stash-add-dialog').modal('hide');
-							var message = 'You have successfully added the collectible to your stash!';
-
-							$.blockUI({
-								message : '<button class="close" data-dismiss="alert" type="button">×</button>' + message,
-								showOverlay : false,
-								css : {
-									top : '100px',
-									'background-color' : '#DDFADE',
-									border : '1px solid #93C49F',
-									'box-shadow' : '3px 3px 5px rgba(0, 0, 0, 0.5)',
-									'border-radius' : '4px 4px 4px 4px',
-									color : '#333333',
-									'margin-bottom' : '20px',
-									padding : '8px 35px 8px 14px',
-									'text-shadow' : '0 1px 0 rgba(255, 255, 255, 0.5)',
-									'z-index' : 999999
-								},
-								timeout : 2000
-							});
-						} else {
-							if (response.response.errors) {
-								stashAddView.errors = response.response.errors;
-								stashAddView.render();
-							}
-						}
-					},
-					error : function(model, xhr, options) {
-
-					}
-				});
-
-			});
 
 			$('#stash-add-dialog').modal();
 		});
