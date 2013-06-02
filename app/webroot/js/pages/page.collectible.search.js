ae9b36e03633f8c5a1606d44f1c36811f006b51f
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2013-06-02 03:12:46 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@1202 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

--- app/webroot/js/pages/page.collectible.search.js
+++ app/webroot/js/pages/page.collectible.search.js
@@ -1,8 +1,41 @@
+var CollectibleView = Backbone.View.extend({
+	template : 'collectible.view',
+	className : 'test',
+	render : function() {
+		var self = this;
+		var data = this.model.toJSON();
+		data.uploadDirectory = uploadDirectory;
+
+		dust.render(this.template, data, function(error, output) {
+			$(self.el).html(output);
+		});
+
+		return this;
+	}
+});
+
+function handleResize() {
+	var sideBarNavWidth = $('.collectible-detail', '#collectibles-list-component').width() - parseInt($('.collectible-detail .well', '#collectibles-list-component').css('paddingLeft')) - parseInt($('.collectible-detail .well', '#collectibles-list-component').css('paddingRight'));
+	$('.collectible-detail .well', '#collectibles-list-component').css('width', sideBarNavWidth);
+}
+
 $(function() {
+	handleResize();
+	$(window).resize(function() {
+		handleResize();
+	});
+
 	// Get all of the data here
 	$.when($.get('/templates/collectibles/collectible.view.dust')).done(function(collectibleTemplate) {
-		dust.loadSource(dust.compile(collectibleTemplate[0], 'collectible.view'));
+		dust.loadSource(dust.compile(collectibleTemplate, 'collectible.view'));
 
+		$('.collectible', '#collectibles-list-component').on('click', function(event) {
+			var collectibleData = $(event.currentTarget).attr('data-collectible');
+
+			$('.collectible-detail .well', '#collectibles-list-component').html(new CollectibleView({
+				model : new Backbone.Model(JSON.parse(collectibleData))
+			}).render().el);
+		});
 	});
 
 });
