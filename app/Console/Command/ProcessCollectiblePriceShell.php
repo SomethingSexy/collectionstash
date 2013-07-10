2c9a54be98057ef6e0080e5f9f742de4fa172d53
cvetant <cvetant@3acffcf6-acfd-4bd8-953e-0dd6b9d92450>
2013-07-10 03:20:07 +0000

    git-svn-id: http://www.collectionstash.com/svn_collectionstash/trunk@1314 3acffcf6-acfd-4bd8-953e-0dd6b9d92450

---
+++ app/Console/Command/ProcessCollectiblePriceShell.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * I plan on having this run once a day.
+ */
+
+class ProcessCollectiblePriceShell extends AppShell {
+	public $uses = array('Collectible', 'CollectiblePriceFact', 'Transaction');
+
+	public function main() {
+		// Trying this for now, use distinct to grab all collectible ids that have transactions
+		
+		$transactions = $this -> Transaction -> find('all', array('contain' => false, 'fields' => array('DISTINCT Transaction.collectible_id')));
+		debug($transactions);
+
+	}
+
+}
+?>
