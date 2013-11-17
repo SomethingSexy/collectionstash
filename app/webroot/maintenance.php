15acdb95040e8aaddb6bdf432abf321db466273f
Tyler Cvetan <tyler.cvetan@gmail.com>
2013-11-17 12:15:19 -0600

    Adding a maintenance page.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

---
+++ app/webroot/maintenance.php
@@ -0,0 +1,36 @@
+<?php
+
+http_response_code(503);
+?>
+<html>
+	<head>
+		<title>Collection Stash</title>
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+		<style>
+			body {
+				color: #444444;
+			}
+			body {
+				background-color: #F1F1F1;
+				overflow-x: hidden;
+				padding-top: 80px;
+			}
+
+		</style>
+		
+		<script src="http://codeorigin.jquery.com/jquery-1.10.2.min.js"></script>
+		
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="page-header">
+				<h1>Collection Stash is down for maintenance.</h1>
+			</div>
+			<p class="lead">
+				We are currently down for maintenance.  We will be back as soon as we can, so you can continue to Stash and earn Nuts.
+			</p>
+			<p>In the mean time, follow us on <a href="https://www.facebook.com/CollectionStash">Facebook</a> and <a href="https://twitter.com/collectionstash">Twitter</a> for updates.</p>
+		</div>
+	</body>
+</html>
