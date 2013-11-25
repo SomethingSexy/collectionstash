b4e186e3656ca06134092f8bd98049dc2ed98a06
Tyler Cvetan <tyler.cvetan@gmail.com>
2013-11-25 15:36:19 -0600

    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Config/config.php
+++ app/Config/config.php
@@ -2,7 +2,7 @@
 $config['Settings'] = Configure::read('Settings');
 
 $config['Settings'] = Set::merge($config['Settings'], array(
-	'version' => '2.9',
+	'version' => '2.10',
   	'title' => 'My Application',
   	'registration' => array(
   		'open' => true,
