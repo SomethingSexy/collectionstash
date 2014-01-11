7acbb52368e5f1baf21acdc1bdde3b51a48104e6
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-01-10 22:38:46 -0600

    Fixed issues with updating collectibles and listings for them.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Lib/Transaction/Type/ExternalTransaction.php
+++ app/Lib/Transaction/Type/ExternalTransaction.php
@@ -41,22 +41,27 @@ class ExternalTransaction extends Object implements Transactionable {
 
 	public function createTransaction($data, $listing, $user) {
 		$transaction = array();
-		$transaction['collectible_id'] = $data['Listing']['collectible_id'];
-		$transaction['sale_price'] = $data['Listing']['current_price'];
-		$transaction['sale_date'] = $data['Listing']['end_date'];
-		$transaction['listing_id'] = $data['Listing']['id'];
+		$transaction['Transaction'] = array();
+		$transaction['Transaction']['collectible_id'] = $data['Listing']['collectible_id'];
+		$transaction['Transaction']['sale_price'] = $data['Listing']['current_price'];
+		$transaction['Transaction']['sale_date'] = $data['Listing']['end_date'];
+		$transaction['Transaction']['listing_id'] = $data['Listing']['id'];
 
 		return $transaction;
-
 	}
-
+	
+	/**
+	 * This method updates a single transaction so return it that way
+	 */
 	public function updateTransaction($data, $listing, $user) {
 
-		$retVal = $listing['Listing']['Transaction'][0];
+		$retVal['Transaction'] = $listing['Listing']['Transaction'][0];
 
 		$retVal['Transaction']['sale_price'] = $data['sold_cost'];
 
 		$retVal['Transaction']['sale_date'] = $data['remove_date'];
+
+		return $retVal;
 	}
 
 }
