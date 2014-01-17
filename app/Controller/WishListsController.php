2ae4ca83f54ce710d0eb7137e30d14ad997d1923
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-01-16 20:31:45 -0600

    Fixed issue with checking stash privacy.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Controller/WishListsController.php
+++ app/Controller/WishListsController.php
@@ -1,34 +1,7 @@
 <?php
 App::uses('Sanitize', 'Utility');
 class WishListsController extends AppController {
-	public $name = 'WishLists';
 	public $helpers = array('Html', 'Form', 'FileUpload.FileUpload', 'Minify', 'Js', 'Time');
-
-	public function updateProfileSettings() {
-		$this -> request -> data = Sanitize::clean($this -> request -> data, array('encode' => false));
-		if ($this -> isLoggedIn()) {
-			if (!empty($this -> request -> data)) {
-				$user = $this -> getUser();
-
-				$stash = $this -> Stash -> find("first", array('conditions' => array('Stash.user_id' => $user['User']['id']), 'contain' => false));
-
-				$this -> Stash -> id = $stash['Stash']['id'];
-				if (!isset($this -> request -> data['Stash']['privacy'])) {
-					$this -> request -> data['Stash']['privacy'] = 0;
-				}
-				if ($this -> Stash -> saveField('privacy', $this -> request -> data['Stash']['privacy'])) {
-					$this -> set('aProfileSettings', array('success' => array('isSuccess' => true, 'message' => __('You have successfully updated your settings.', true))));
-				} else {
-					$this -> set('aProfileSettings', array('success' => array('isSuccess' => false), 'isTimeOut' => false, 'errors' => array($this -> Stash -> validationErrors)));
-				}
-			} else {
-				$this -> set('aProfileSettings', array('success' => array('isSuccess' => false), 'isTimeOut' => false, 'message' => array('There was an issue trying to save your settings.')));
-			}
-
-		} else {
-			$this -> set('aProfileSettings', array('success' => array('isSuccess' => false), 'isTimeOut' => true));
-		}
-	}
 
 	public function view($userId = null, $type = 'tile') {
 		$this -> layout = 'fluid';
@@ -48,7 +21,7 @@ class WishListsController extends AppController {
 					$this -> set('stashUsername', $userId);
 					//If the privacy is 0 or you are viewing your own stash then always show
 					//or if it is set to 1 and this person is logged in also show.
-					if ($user['Stash']['privacy'] === '0' || $viewingMyStash || ($user['Stash']['privacy'] === '1' && $this -> isLoggedIn())) {
+					if ($user['Stash'][0]['privacy'] === '0' || $viewingMyStash || ($user['Stash'][0]['privacy'] === '1' && $this -> isLoggedIn())) {
 						$this -> paginate = array('joins' => array( array('alias' => 'Stash', 'table' => 'stashes', 'type' => 'inner', 'conditions' => array('Stash.id = CollectiblesWishList.wish_list_id'))), 'limit' => 25, 'order' => array('sort_number' => 'desc'), 'conditions' => array('CollectiblesWishList.user_id' => $user['User']['id']), 'contain' => array('Collectible' => array('User', 'CollectiblesUpload' => array('Upload'), 'Manufacture', 'Collectibletype')));
 						$collectibles = $this -> paginate('CollectiblesWishList');
 
