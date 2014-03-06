955d5b5e93e9269cd8266c6fe3abd608def319c6
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-03-05 21:22:24 -0600

    #33
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- deploy/collectionstash_deploy.sh
+++ deploy/collectionstash_deploy.sh
@@ -6,6 +6,9 @@ cp  -r /usr/src/collectionstash/app/* /var/www/collectionstash/app;
 cp  -r /usr/src/collectionstash/plugins/* /var/www/collectionstash/plugins;
 cp  -r /usr/src/collectionstash/vendors/* /var/www/collectionstash/vendors;
 
+# delete cache folders
+rm -rf /var/www/collectionstash/app/tmp/cache/collectible/*;
+rm -rf /var/www/collectionstash/app/tmp/cache/models/*;
 # restart web servers
 /etc/init.d/apache2 restart;
 /etc/init.d/nginx restart;
