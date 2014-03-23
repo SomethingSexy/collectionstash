be91b3a565b153c682840844b5135a8dc8aae84c
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-03-23 13:19:06 -0500

    #48 Testing
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Test/Fixture/EntityTypeFixture.php
+++ app/Test/Fixture/EntityTypeFixture.php
@@ -5,5 +5,6 @@ class EntityTypeFixture extends CakeTestFixture {
 	// Set this property to load fixtures to a different test datasource
 	public $useDbConfig = 'test';
 	public $import = array('model' => 'EntityType');
+	public $records = array( array('id' => 1, 'type' => 'collectible', 'created' => '2007-03-18 10:39:23', 'modified' => '2007-03-18 10:41:31'));
 }
 ?>
--- app/Test/Fixture/RevisionFixture.php
+++ app/Test/Fixture/RevisionFixture.php
@@ -5,5 +5,6 @@ class RevisionFixture extends CakeTestFixture {
 	// Set this property to load fixtures to a different test datasource
 	public $useDbConfig = 'test';
 	public $import = array('model' => 'Revision');
+	public $records = array( array('id' => 1, 'user_id' => 1, 'action' => 'A', 'created' => '2007-03-18 10:39:23', 'modified' => '2007-03-18 10:41:31'));
 }
 ?>
