168da8f8afbe9b13998cbe62718e4800f9374c86
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-03-24 20:18:00 -0500

    Testing fix for update to Listing.  Made it hasOne relationship.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

---
+++ app/Test/Case/Model/CollectiblesUserTest.php
@@ -0,0 +1,27 @@
+<?php
+App::uses('CollectiblesUser', 'Model');
+class CollectiblesUserTest extends CakeTestCase {
+	// ugh these are all pretty much needed for collectible remove since it has to remove a lot of stuff
+	public $fixtures = array('app.collectible', 'app.user', 'app.listing', 'app.collectibles_user', 'app.merchant', 'app.collectible_user_remove_reason', 'app.condition', 'app.transaction', 'app.stash');
+
+	public function setUp() {
+		parent::setUp();
+		$this -> CollectiblesUser = ClassRegistry::init('CollectiblesUser');
+
+	}
+
+	/**
+	 * Testing admin remove of a single collectible
+	 */
+	public function testFind() {
+		$result = $this -> CollectiblesUser -> find('first', array('conditions' => array('CollectiblesUser.id' => 1), 'contain' => array('Condition', 'Merchant', 'User', 'Stash', 'Listing')));
+		$this -> assertNotEmpty($result);
+
+		$this -> assertNotEmpty($result['Listing']);
+		$this -> assertNotEmpty($result['Listing']['id']);
+		$this -> assertEqual($result['Listing']['listing_price'], '10.00');
+
+	}
+
+}
+?>
--- app/Test/Fixture/ListingFixture.php
+++ app/Test/Fixture/ListingFixture.php
@@ -5,6 +5,8 @@ class ListingFixture extends CakeTestFixture {
 	// Set this property to load fixtures to a different test datasource
 	public $useDbConfig = 'test';
 	public $import = 'Listing';
-	public $records = array();
+	public $records = array(
+	//
+	array('id' => '51b938ad-e988-4011-999e-555d4537ee41', 'user_id' => 1, 'collectible_id' => 1, 'listing_type_id' => 2, 'listing_price' => '10.00'));
 }
 ?>
