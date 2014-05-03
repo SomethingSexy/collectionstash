187aa025bd2375c588309f97f5ae67011844275f
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-05-02 22:14:54 -0500

    #66 Adding stash settings
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -80,7 +80,7 @@ Router::connect('/manufacturer/*', array('controller' => 'manufactures', 'action
 Router::connect('/artist/*', array('controller' => 'artists', 'action' => 'index'));
 // logged in user profile
 Router::connect('/profile', array('controller' => 'profiles', 'action' => 'index'));
-Router::connect('/profile/privacy', array('controller' => 'profiles', 'action' => 'privacy'));
+Router::connect('/profile/privacy', array('controller' => 'profiles', 'action' => 'index'));
 Router::connect('/profile/*', array('controller' => 'users', 'action' => 'profile'));
 
 
