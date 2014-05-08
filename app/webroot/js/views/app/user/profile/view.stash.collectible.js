529ef2bb20efe3c6872008d5ed29a20edd1aea70
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-05-08 15:58:25 -0500

    #27 Getting wookmark in to handle stash view
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/webroot/js/views/app/user/profile/view.stash.collectible.js
+++ app/webroot/js/views/app/user/profile/view.stash.collectible.js
@@ -1,6 +1,7 @@
 define(['require', 'marionette', 'text!templates/app/user/profile/stash.collectible.mustache', 'mustache', 'marionette.mustache'], function(require, Marionette, template, mustache) {
 
     return Marionette.ItemView.extend({
+        className: 'tile stash-item col-xs-6 col-md-3',
         template: template
     });
 });
--- app/webroot/js/views/app/user/profile/view.stash.js
+++ app/webroot/js/views/app/user/profile/view.stash.js
@@ -1,8 +1,30 @@
-define(['require', 'marionette', 'text!templates/app/user/profile/stash.mustache', 'views/app/user/profile/view.stash.collectible', 'mustache', 'marionette.mustache'], function(require, Marionette, template, CollectibleView, mustache) {
+define(['require', 'marionette', 'text!templates/app/user/profile/stash.mustache', 'views/app/user/profile/view.stash.collectible', 'mustache', 'imagesloaded', 'wookmark',
+    'marionette.mustache'
+], function(require, Marionette, template, CollectibleView, mustache, Masonry) {
 
     return Marionette.CompositeView.extend({
         template: template,
         itemView: CollectibleView,
         itemViewContainer: "._tiles",
+        onRender: function() {
+            var handler = $('._tiles .tile', this.el);
+            $('._tiles', this.el).imagesLoaded(function() {
+                // Call the layout function.
+                handler.wookmark({
+                    autoResize: true, // This will auto-update the layout when the browser window is resized.
+                    container: $('._tiles', this.el)
+                });
+
+                // Capture clicks on grid items.
+                handler.click(function() {
+                    // Randomize the height of the clicked item.
+                    var newHeight = $('img', this).height() + Math.round(Math.random() * 300 + 30);
+                    $(this).css('height', newHeight + 'px');
+
+                    // Update the layout.
+                    handler.wookmark();
+                });
+            });
+        }
     });
 });
