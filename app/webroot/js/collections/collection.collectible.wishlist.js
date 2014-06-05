d5df8dd2bc04be8f2ba24c3dc35125716c401c7c
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-06-04 21:33:34 -0500

    Starting to add wish list
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

---
+++ app/webroot/js/collections/collection.collectible.wishlist.js
@@ -0,0 +1,31 @@
+define(['backbone', 'backbone.pageable', 'models/model.collectible.user'], function(Backbone, pageable, CollectibleUserModel) {
+    return Backbone.PageableCollection.extend({
+        model: CollectibleUserModel,
+        initialize: function(models, props) {
+            this.url = "/collectibles_wishlists/collectibles/" + props.username;
+        },
+        mode: "infinite",
+        state: {
+            pageSize: 25,
+            sortKey: "updated",
+            order: 1,
+            query: {}
+        },
+        queryParams: {
+            query: function() {
+                return this.state.query;
+            },
+        },
+        setQuery: function(query, page_size) {
+            var state = this.state;
+            if (query != state.query) {
+                state = _.clone(this._initState)
+                //state.pageSize = page_size;
+            }
+            state = this.state = this._checkState(_.extend({}, state, {
+                query: query,
+            }));
+            
+        }
+    });
+});
