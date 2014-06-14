b8bd58aebab8c3b53b9438b3af92e9555c549fc6
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-06-14 09:49:42 -0500

    Adding in error mixin for form views.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/webroot/js/views/common/view.comment.add.js
+++ app/webroot/js/views/common/view.comment.add.js
@@ -1,57 +1,22 @@
-define(['require', 'backbone', 'marionette', 'text!templates/app/common/comment.add.mustache', 'mustache', 'marionette.mustache'], function(require, Backbone, Marionnette, template) {
+define(['require', 'backbone', 'marionette', 'text!templates/app/common/comment.add.mustache', 'views/common/mixin.error', 'mustache', 'marionette.mustache'], function(require, Backbone, Marionnette, template, ErrorMixin) {
 
-    return Marionnette.ItemView.extend({
+    var CommentAdd = Marionnette.ItemView.extend({
         template: template,
         events: {
-            // TODO Add event for change of reason
             'click .save': 'save'
         },
         initialize: function(options) {
-            // this.collectible = options.collectible;
-            // this.reasons = options.reasons;
-
             this.model.startTracking();
         },
         onRender: function() {
             var self = this;
             this.errors = [];
         },
-        // serializeData: function() {
-        //     var data = this.model.toJSON();
-        //     data.Collectible = this.model.collectible.toJSON();
-        //     return data;
-        // },
         onClose: function() {
             this.model.resetAttributes();
             this.model.stopTracking();
         },
-        onError: function() {
-            $('.btn-primary', this.el).button('reset');
-            this.removeErrors();
-            var self = this;
-            _.each(this.errors, function(error, attr) {
-                $('[name="' + attr + '"]', self.el).addClass('invalid').attr('data-error', true);
-                $('[name="' + attr + '"]', self.el).closest('.form-group').addClass('has-error');
-                $('[name="' + attr + '"]', self.el).parent().find('._error').remove();
-                var errorHtml = '';
-                if (_.isArray(error)) {
-                    if (error.length === 1) {
-                        errorHtml = error[0];
-                    } else {
-                        _.each(error, function(message) {
-                            errorHtml += '<p>' + message + '</p>';
-                        });
-                    }
-                } else {
-                    errorHtml = error;
-                }
 
-                $('[name="' + attr + '"]', self.el).after('<span class="help-block _error">' + errorHtml + '</span>');
-            });
-        },
-        removeErrors: function() {
-            $('input[data-error=true]', this.el).removeClass('invalid').closest('.form-group').removeClass('has-error').children('._error').empty();
-        },
         // TODO: update this to do what we did for the profile
         // only set the fields when we do the save...taht way if they
         // cancel we won't have to worry about remove values
@@ -64,7 +29,7 @@ define(['require', 'backbone', 'marionette', 'text!templates/app/common/comment.
             var data = {
                 'comment': $('[name=comment]', this.el).val(),
             };
-            
+
             $('.btn-primary', this.el).button('loading');
 
             this.model.save(data, {
@@ -73,13 +38,13 @@ define(['require', 'backbone', 'marionette', 'text!templates/app/common/comment.
                     $('.btn-primary', self.el).button('reset');
                 },
                 error: function(model, response, options) {
-                    $('.btn-primary', self.el).button('reset');
-                    self.errors = response.responseJSON;
-                    self.onError();
+                    self.onModelError(model, response, options);
                 }
             });
         }
     });
 
-    return StashRemoveView;
+    _.extend(CommentAdd.prototype, ErrorMixin);
+
+    return CommentAdd;
 });
