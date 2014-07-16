d6be8209b33a305d2d8a5c7cd76b326d6bb42dc7
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-07-15 20:24:36 -0500

    more work on submissions.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/webroot/js/collections/collection.collectible.history.js
+++ app/webroot/js/collections/collection.collectible.history.js
@@ -1,6 +1,13 @@
 define(['backbone', 'backbone.pageable'], function(Backbone, pageable) {
-     return Backbone.PageableCollection.extend({
+    return Backbone.PageableCollection.extend({
         model: Backbone.Model,
+        queryParams: {
+            query: function() {
+                return this.state.query;
+            },
+            "sortKey": "sort",
+            "order": "direction"
+        },
         initialize: function(models, props) {
             this.url = "/collectibles/userHistory/" + props.username;
         }
--- app/webroot/js/views/app/user/profile/view.submissions.js
+++ app/webroot/js/views/app/user/profile/view.submissions.js
@@ -15,13 +15,61 @@ define(['require', 'marionette', 'text!templates/app/user/profile/submissions.mu
                 permissions: this.permissions
             };
         },
+        serializeData: function() {
+            var data = {
+                showPagination: this.collection.state.totalPages > 1
+            };
+            return data;
+        },
+        sorts: {
+            'Collectible.status_id': -1,
+            'created': -1,
+            'id': -1,
+            'name': -1,
+            'Collectible.collectibletype_id': -1
+        },
         _initialEvents: function() {
             this.listenTo(this.collection, "remove", this.removeItemView);
             this.listenTo(this.collection, "reset", this.render);
             this.listenTo(this.collection, "sync", this.renderMore);
         },
+        events: {
+            'click ._sort': 'sort'
+        },
         initialize: function(options) {
-        	this.permissions = options.permissions;
+            this.permissions = options.permissions;
+        },
+        renderMore: function() {
+            var self = this;
+            var ItemView;
+
+            $(this.itemViewContainer, this.el).empty();
+
+            this.startBuffering();
+            this.collection.each(function(item, index) {
+                ItemView = this.getItemView(item);
+                this.addItemView(item, ItemView, index);
+            }, this);
+            this.endBuffering();
+
+            $('._pagination', this.el).pagination('updateItems', this.collection.state.totalRecords);
+        },
+        onRender: function() {
+            var self = this;
+            $('._pagination', this.el).pagination({
+                items: this.collection.state.totalRecords,
+                itemsOnPage: this.collection.state.pageSize,
+                cssStyle: 'pagination',
+                onPageClick: function(pageNumber, event) {
+                    self.collection.getPage(pageNumber);
+                }
+            });
+        },
+        sort: function(event) {
+            var sort = $(event.currentTarget).data('sort');
+            this.sorts[sort] = this.sorts[sort] === -1 ? 1 : -1;
+            this.collection.setSorting(sort, this.sorts[sort]);
+            this.collection.fetch();
         }
     });
 });
