62cc421d0693110a125841d2a85b39481966f364
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-08-02 13:49:48 -0500

    #77 updating delpoy script
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- deploy/collectionstash_deploy.sh
+++ deploy/collectionstash_deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd /usr/src/collectionstash;
 git pull;
+npm install;
+grunt install;
 # copy over source
 cp  -r /usr/src/collectionstash/app/* /var/www/collectionstash/app;
 cp  -r /usr/src/collectionstash/plugins/* /var/www/collectionstash/plugins;
 cp  -r /usr/src/collectionstash/vendors/* /var/www/collectionstash/vendors;
-
 # delete cache folders
 rm -rf /var/www/collectionstash/app/tmp/cache/collectible/*;
 rm -rf /var/www/collectionstash/app/tmp/cache/models/*;
