7a18dfe2a667a47910e5ca6e89c3bee9bb02c89d
Tyler Cvetan <tyler.cvetan@gmail.com>
2014-08-02 10:33:19 -0500

    Deleting code we don't need and fixing routes.
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -12,42 +12,39 @@ class HomeController extends AppController
      * they will see the catalog page.
      */
     public function index() {
-        if ($this->isLoggedIn()) {
-            $this->redirect(array('controller' => 'user', 'action' => 'home'));
-        } else {
-            $this->loadModel('Collectible');
-            // Now grab the pending collectible
-            $pending = $this->Collectible->getPendingCollectibles(array('limit' => 4, 'order' => array('Collectible.created' => 'desc')));
-            $totalPending = $this->Collectible->getNumberOfPendingCollectibles();
-            
-            $extractPending = Set::extract('/Collectible/.', $pending);
-            
-            foreach ($extractPending as $key => $value) {
-                $extractPending[$key]['CollectiblesUpload'] = $pending[$key]['CollectiblesUpload'];
-            }
-            
-            $this->set('pending', $extractPending);
-            
-            $this->set(compact('totalPending'));
-            
-            $totalNew = $this->Collectible->find('count', array('conditions' => array('Collectible.status_id' => 4), 'limit' => 4));
-            $newCollectibles = $this->Collectible->find('all', array('conditions' => array('Collectible.status_id' => 4), 'order' => array('Collectible.modified' => 'desc'), 'contain' => array('User', 'Collectibletype', 'Manufacture', 'Status', 'CollectiblesUpload' => array('Upload')), 'limit' => 4));
-            
-            $extractNewCollectibles = Set::extract('/Collectible/.', $newCollectibles);
-            
-            foreach ($extractNewCollectibles as $key => $value) {
-                $extractNewCollectibles[$key]['CollectiblesUpload'] = $newCollectibles[$key]['CollectiblesUpload'];
-            }
-            $this->set('newCollectibles', $extractNewCollectibles);
-            $this->set(compact('totalNew'));
-            
-            $this->loadModel('Activity');
-            $activity = $this->Activity->find('all', array('limit' => 10, 'order' => array('Activity.created' => 'desc')));
-            $this->set('activity', Set::extract('/Activity/.', $activity));
-            $totalActivity = $this->Activity->find('count');
-            $this->set(compact('totalActivity'));
-            $this->layout = 'require';
+        
+        $this->loadModel('Collectible');
+        // Now grab the pending collectible
+        $pending = $this->Collectible->getPendingCollectibles(array('limit' => 4, 'order' => array('Collectible.created' => 'desc')));
+        $totalPending = $this->Collectible->getNumberOfPendingCollectibles();
+        
+        $extractPending = Set::extract('/Collectible/.', $pending);
+        
+        foreach ($extractPending as $key => $value) {
+            $extractPending[$key]['CollectiblesUpload'] = $pending[$key]['CollectiblesUpload'];
         }
+        
+        $this->set('pending', $extractPending);
+        
+        $this->set(compact('totalPending'));
+        
+        $totalNew = $this->Collectible->find('count', array('conditions' => array('Collectible.status_id' => 4), 'limit' => 4));
+        $newCollectibles = $this->Collectible->find('all', array('conditions' => array('Collectible.status_id' => 4), 'order' => array('Collectible.modified' => 'desc'), 'contain' => array('User', 'Collectibletype', 'Manufacture', 'Status', 'CollectiblesUpload' => array('Upload')), 'limit' => 4));
+        
+        $extractNewCollectibles = Set::extract('/Collectible/.', $newCollectibles);
+        
+        foreach ($extractNewCollectibles as $key => $value) {
+            $extractNewCollectibles[$key]['CollectiblesUpload'] = $newCollectibles[$key]['CollectiblesUpload'];
+        }
+        $this->set('newCollectibles', $extractNewCollectibles);
+        $this->set(compact('totalNew'));
+        
+        $this->loadModel('Activity');
+        $activity = $this->Activity->find('all', array('limit' => 10, 'order' => array('Activity.created' => 'desc')));
+        $this->set('activity', Set::extract('/Activity/.', $activity));
+        $totalActivity = $this->Activity->find('count');
+        $this->set(compact('totalActivity'));
+        $this->layout = 'require';
     }
 }
 ?>
