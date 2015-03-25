2fd5d2f589f7effbf25e9a443bca18c3cdb8fe6f
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-03-24 21:22:58 -0500

    #197
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

---
+++ app/Lib/Collectible.php
@@ -0,0 +1,30 @@
+<?php
+class Collectible
+{
+    // property declaration
+    public $name;
+    public $manufacturer;
+    public $collectibleType;
+    public $description;
+    public $msrp;
+    public $editionSize;
+    public $upc;
+    public $width;
+    public $height;
+    public $depth;
+    public $weight;
+    public $brand;
+    public $variant = false;
+    public $url;
+    public $exclusive = false;
+    public $scale;
+    public $releaseYear;
+    public $limited = false;
+    public $productCode;
+    public $numbered = false;
+    public $numberOfPieces;
+    public $signed = false;
+    public $official = true;
+    public $photos = array();
+}
+?>
