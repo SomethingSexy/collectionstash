37183127ffb520e97318b37141e750232a6d4d2b
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-04-16 20:40:32 -0500

    prepping release
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/Config/config.php
+++ app/Config/config.php
@@ -2,7 +2,7 @@
 $config['Settings'] = Configure::read('Settings');
 
 $config['Settings'] = Set::merge($config['Settings'], array(
-	'version' => '3.12.0',
+	'version' => '4.0.0',
   	'title' => 'My Application',
   	'domain' => 'http://collectionstash.com',
   	'registration' => array(
